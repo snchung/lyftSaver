fd5484284441d40052a47753c22a4b5fb08022db
snchung <chungsa@seas.upenn.edu>
2016-09-10 13:55:16 -0400

    trip object

---
+++ lyftSaver/src/lyftSaver/TripObject.java
@@ -0,0 +1,11 @@
+package lyftSaver;
+
+public class TripObject {
+
+	private double startingLong, startingLat, endingLong, endingLat;
+	private double cost;
+	
+	public TripObject(double startLong, double startLat, double endLong, double endLat) {
+		
+	}
+}
