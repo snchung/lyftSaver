f66384030fb679acb38bc2985f6f89737e65e761
tstat1996 <tstat@seas.upenn.edu>
2016-09-10 13:05:27 -0400

    url

--- lyftSaver/src/lyftSaver/ImportAPI.java
+++ lyftSaver/src/lyftSaver/ImportAPI.java
@@ -1,6 +1,19 @@
 package lyftSaver;
 
+import java.net.*;
+import java.io.*;
+
 public class ImportAPI {
 
+	public static void main(String[] args) throws Exception{
+		URL u = new URL("https://api.lyft.com/oauth/authorize");
+		URLConnection uc = u.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+		String inputLine;
+		while((inputLine = in.readLine()) != null){
+			System.out.println(inputLine);
+		}
+		in.close();
+	}
 	
 }
